2d7f51c04d16243d20d6c84014733117f24ae7bc
shash123-d <88714649+shash123-d@users.noreply.github.com>
2026-02-24 15:19:21 -0800

    Added bot solver

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const main = document.querySelector('main');
-    const diff = document.getElementById('difficulty-select');
-    const gridElem = document.getElementById('grid');
+    const gridElem = document.getElementById('grid')
+    const diff = document.getElementById('difficulty-select');;
     const flagsleft = document.getElementById('nflags');
     
     const resetbtn = document.getElementById('reset-game-button');
     const newgamebtn = document.getElementById('new-game-button');
+    const botplaybtn = document.getElementById('solve-game-button');
     resetbtn.addEventListener('click', reset);
     newgamebtn.addEventListener('click', newgame);
+    botplaybtn.addEventListener('click', solve);
 
     const minesound = new Audio('assets/tick.mp3');
     const flagsound = new Audio('assets/flag.mp3');
@@ -37,15 +38,15 @@ window.addEventListener('DOMContentLoaded', () => {
         if (diff.value == 'easy') {
             gridSize = 10
             nmines = 15
-            main.style.setProperty('--grid-size', 10)
+            gridElem.style.setProperty('--grid-size', 10)
         } else if (diff.value == 'medium') {
             gridSize = 18
             nmines = 40
-            main.style.setProperty('--grid-size', 18)
+            gridElem.style.setProperty('--grid-size', 18)
         } else if (diff.value == 'hard') {
             gridSize = 24
             nmines = 99
-            main.style.setProperty('--grid-size', 24)
+            gridElem.style.setProperty('--grid-size', 24)
         }
         flagsleft.innerText = nmines;
     }
@@ -195,15 +196,16 @@ window.addEventListener('DOMContentLoaded', () => {
             startCell.innerText = '';
             startCell.style.color = '';
         }
-        const x = Number(cell.dataset.x);
-        const y = Number(cell.dataset.y);
-        mine(cell, x, y)
+        mine(cell)
     }
 
     function mine(cell, x, y, visited = new Set([])) {
         if (gameOver) { return }
         if (!cell) return;
         if (!(cell.classList.contains('unmined'))) { return }
+
+        x = x || Number(cell.dataset.x);
+        y = y || Number(cell.dataset.y);
 
         if (grid[x][y] === 9) {
             // clicked mine
@@ -248,6 +250,7 @@ window.addEventListener('DOMContentLoaded', () => {
             confetti.addConfetti();
             return;
         }
+
         minesound.play();
     }
 
@@ -286,18 +289,178 @@ window.addEventListener('DOMContentLoaded', () => {
             unflagsound.play();
             cell.classList.remove('flagged')
             nflags--;
-            cell.classList.add('unmined')
             cell.addEventListener('click', click)
         } else {
             flagsound.play();
             nflags++;
-            cell.classList.remove('unmined')
             cell.classList.add('flagged')
             cell.removeEventListener('click', click);
         }
         flagsleft.innerText = nmines - nflags;
     }
+
+    const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+    async function solve() {
+        resetbtn.removeEventListener('click', reset);
+        newgamebtn.removeEventListener('click', newgame);
+
+        const visibleGrid = Array.from({ length: gridSize }, () => [])
+        const cellstack = [];
+        if (mined == 0) {
+            startCell.classList.add('hover');
+            await sleep(500);
+            startCell.innerText = '';
+            startCell.style.color = '';
+            startCell.classList.remove('hover');
+            mine(startCell);
+        }
+
+        for (let i = 0; i < gridSize; i++) {
+            for (let j = 0; j < gridSize; j++) {
+                if (!(cells[i][j].classList.contains('unmined'))) {
+                    visibleGrid[i].push(grid[i][j])
+                    if (grid[i][j] != 0) cellstack.push(cells[i][j]);
+                } else {
+                    if (cells[i][j].classList.contains('flagged')) flag(cells[i][j]); // unflag
+                    visibleGrid[i].push('D')
+                }
+            }
+        }
+
+        console.log('What the bot sees: ');
+        console.log(visibleGrid);
+
+        const delay = 100
+        let stuck = false;
+
+        while (!stuck) {
+            stuck = true;
+            let ind = 0;
+            console.log('cellstack: ')
+            console.log(cellstack)
+            while (ind < cellstack.length) {
+                const cell = cellstack[ind]
+                console.log('checking cell:')
+                console.log(cell)
+                const x = Number(cell.dataset.x)
+                const y = Number(cell.dataset.y)
+                const n = visibleGrid[x][y]
+                console.log(x, y)
+                console.log(n)
+                let unmined = []
+                let flags = 0
+                for (const [nx, ny] of neighbours(x, y)) {
+                    if (visibleGrid[nx][ny] == 'D') unmined.push([nx, ny]);
+                    if (visibleGrid[nx][ny] == 'F') flags++;
+                }
+                console.log('unmined:')
+                console.log(unmined)
+                console.log(`flags: ${flags}`)
+                console.log()
+
+                // Basic deduction
+                if ((unmined.length+flags)== n) {
+                    for (const [nx, ny] of unmined) {
+                        console.log('flagging neighbours')
+                        visibleGrid[nx][ny] = 'F';
+                        flag(cells[nx][ny]);
+                        stuck = false;
+                        await sleep(delay);
+                    }
+                    console.log('removing cell from cellstack')
+                    cellstack.splice(ind, 1)
+                    ind--;
+                } else if (flags == n) {
+                    for (const [nx, ny] of unmined) {
+                        if (!(cells[nx][ny].classList.contains('unmined'))) { continue }
+                        console.log('mining neighbours')
+                        cells[nx][ny].classList.add('hover')
+                        await sleep(delay)
+                        cells[nx][ny].classList.remove('hover')
+                        mine(cells[nx][ny], nx, ny)
+                        visibleGrid[nx][ny] = grid[nx][ny]
+                        if (grid[nx][ny] != 0) {
+                            cellstack.push(cells[nx][ny]);
+                        } else {
+                            updateVisibleGridDFS(`${nx} ${ny}`)
+                        }
+                        stuck = false;
+                        await sleep(delay)
+                    }
+                }
+                ind++;
+            }
+            console.log('cellstack after iterating: ')
+            console.log(cellstack)
+            console.log('visible grid after iterating: ')
+            console.log(visibleGrid)
+            
+            if (stuck & (!gameOver)) {
+                // Guessing
+                console.log('FINDING BEST GUESS');
+                console.log(cellstack)
+                let best = 0
+                let bc = null; 
+                for (const cell of cellstack) {
+                    const x = Number(cell.dataset.x)
+                    const y = Number(cell.dataset.y)
+                    const n = visibleGrid[x][y]
+                    let unmined = []
+                    let flags = 0
+                    for (const [nx, ny] of neighbours(x, y)) {
+                        if (visibleGrid[nx][ny] == 'D') unmined.push([nx, ny]);
+                        if (visibleGrid[nx][ny] == 'F') flags++;
+                    }
+                    if (((unmined.length+flags) - n) > best) {
+                        // Best possible guess
+                        best = (unmined.length+flags) - n
+                        bc = unmined[0]
+                        break;
+                    }
+                }
+                cells[bc[0]][bc[1]].classList.add('hover')
+                await sleep(delay)
+                cells[bc[0]][bc[1]].classList.remove('hover')
+                mine(cells[bc[0]][bc[1]], bc[0], bc[1])
+                visibleGrid[bc[0]][bc[1]] = grid[bc[0]][bc[1]]
+                if (grid[bc[0]][bc[1]] != 0) {
+                    cellstack.push(cells[bc[0]][bc[1]]);
+                } else {
+                    updateVisibleGridDFS(`${bc[0]} ${bc[1]}`)
+                }
+                stuck = false;
+
+                // TODO: Maybe pattern recognition (Eg: "1-2-1", "1-1", etc.)
+                
+                // TODO: Constraint Satisfaction 
+
+                // TODO: Better probability guessing
+
+            }
+        }
+        console.log('finished game')
+
+        function updateVisibleGridDFS(coords, visited = new Set([])) {
+            visited.add(coords);
+            console.log(structuredClone(visited))
+            const split = coords.split(" ")
+            const x = Number(split[0])
+            const y = Number(split[1])
+            for (const [nx, ny] of neighbours(x, y)) {
+                if (visited.has(`${nx} ${ny}`)) continue
+                if (visibleGrid[nx][ny] != 'D') continue
+                if (cells[nx][ny].classList.contains('unmined')) continue
+                visibleGrid[nx][ny] = grid[nx][ny]
+                console.log(`assigned ${nx} ${ny} to ${grid[nx][ny]}`)
+                if (grid[nx][ny] != 0) cellstack.push(cells[nx][ny])
+                updateVisibleGridDFS(`${nx} ${ny}`, visited)
+            }
+        }
+
+        resetbtn.addEventListener('click', reset);
+        newgamebtn.addEventListener('click', newgame);
+    }
 });
 
 var JSConfetti=function(){"use strict";function t(t,i){if(!(t instanceof i))throw new TypeError("Cannot call a class as a function")}function i(t,i){for(var e=0;e<i.length;e++){var o=i[e];o.enumerable=o.enumerable||!1,o.configurable=!0,"value"in o&&(o.writable=!0),Object.defineProperty(t,o.key,o)}}function e(t,e,o){return e&&i(t.prototype,e),o&&i(t,o),t}function o(t){return+t.replace(/px/,"")}function n(t,i){var e=arguments.length>2&&void 0!==arguments[2]?arguments[2]:0,o=Math.random()*(i-t)+t;return Math.floor(o*Math.pow(10,e))/Math.pow(10,e)}function s(t){return t[n(0,t.length)]}var a=["#fcf403","#62fc03","#f4fc03","#03e7fc","#03fca5","#a503fc","#fc03ad","#fc03c2"];function r(t){return Math.log(t)/Math.log(1920)}var h=function(){function i(e){t(this,i);var o=e.initialPosition,a=e.direction,h=e.confettiRadius,c=e.confettiColors,d=e.emojis,l=e.emojiSize,u=e.canvasWidth,f=n(.9,1.7,3)*r(u);this.confettiSpeed={x:f,y:f},this.finalConfettiSpeedX=n(.2,.6,3),this.rotationSpeed=d.length?.01:n(.03,.07,3)*r(u),this.dragForceCoefficient=n(5e-4,9e-4,6),this.radius={x:h,y:h},this.initialRadius=h,this.rotationAngle="left"===a?n(0,.2,3):n(-.2,0,3),this.emojiSize=l,this.emojiRotationAngle=n(0,2*Math.PI),this.radiusYUpdateDirection="down";var p="left"===a?n(82,15)*Math.PI/180:n(-15,-82)*Math.PI/180;this.absCos=Math.abs(Math.cos(p)),this.absSin=Math.abs(Math.sin(p));var m=n(-150,0),v={x:o.x+("left"===a?-m:m)*this.absCos,y:o.y-m*this.absSin};this.currentPosition=Object.assign({},v),this.initialPosition=Object.assign({},v),this.color=d.length?null:s(c),this.emoji=d.length?s(d):null,this.createdAt=(new Date).getTime(),this.direction=a}return e(i,[{key:"draw",value:function(t){var i=this.currentPosition,e=this.radius,o=this.color,n=this.emoji,s=this.rotationAngle,a=this.emojiRotationAngle,r=this.emojiSize,h=window.devicePixelRatio;o?(t.fillStyle=o,t.beginPath(),t.ellipse(i.x*h,i.y*h,e.x*h,e.y*h,s,0,2*Math.PI),t.fill()):n&&(t.font="".concat(r,"px serif"),t.save(),t.translate(h*i.x,h*i.y),t.rotate(a),t.textAlign="center",t.fillText(n,0,0),t.restore())}},{key:"updatePosition",value:function(t,i){var e=this.confettiSpeed,o=this.dragForceCoefficient,n=this.finalConfettiSpeedX,s=this.radiusYUpdateDirection,a=this.rotationSpeed,r=this.createdAt,h=this.direction,c=i-r;e.x>n&&(this.confettiSpeed.x-=o*t),this.currentPosition.x+=e.x*("left"===h?-this.absCos:this.absCos)*t,this.currentPosition.y=this.initialPosition.y-e.y*this.absSin*c+.00125*Math.pow(c,2)/2,this.rotationSpeed-=this.emoji?1e-4:1e-5*t,this.rotationSpeed<0&&(this.rotationSpeed=0),this.emoji?this.emojiRotationAngle+=this.rotationSpeed*t%(2*Math.PI):"down"===s?(this.radius.y-=t*a,this.radius.y<=0&&(this.radius.y=0,this.radiusYUpdateDirection="up")):(this.radius.y+=t*a,this.radius.y>=this.initialRadius&&(this.radius.y=this.initialRadius,this.radiusYUpdateDirection="down"))}},{key:"getIsVisibleOnCanvas",value:function(t){return this.currentPosition.y<t+100}}]),i}();function c(){var t=document.createElement("canvas");return t.style.position="fixed",t.style.width="100%",t.style.height="100%",t.style.top="0",t.style.left="0",t.style.zIndex="1000",t.style.pointerEvents="none",document.body.appendChild(t),t}function d(t){var i=t.confettiRadius,e=void 0===i?6:i,o=t.confettiNumber,n=void 0===o?t.confettiesNumber||(t.emojis?40:250):o,s=t.confettiColors,r=void 0===s?a:s,h=t.emojis,c=void 0===h?t.emojies||[]:h,d=t.emojiSize,l=void 0===d?80:d;return t.emojies&&console.error("emojies argument is deprecated, please use emojis instead"),t.confettiesNumber&&console.error("confettiesNumber argument is deprecated, please use confettiNumber instead"),{confettiRadius:e,confettiNumber:n,confettiColors:r,emojis:c,emojiSize:l}}return function(){function i(){var e=arguments.length>0&&void 0!==arguments[0]?arguments[0]:{};t(this,i),this.canvas=e.canvas||c(),this.canvasContext=this.canvas.getContext("2d"),this.shapes=[],this.lastUpdated=(new Date).getTime(),this.iterationIndex=0,this.loop=this.loop.bind(this),requestAnimationFrame(this.loop)}return e(i,[{key:"loop",value:function(){var t,i,e,n,s,a=this;t=this.canvas,i=window.devicePixelRatio,e=getComputedStyle(t),n=o(e.getPropertyValue("width")),s=o(e.getPropertyValue("height")),t.setAttribute("width",(n*i).toString()),t.setAttribute("height",(s*i).toString());var r=(new Date).getTime(),h=r-this.lastUpdated,c=this.canvas.offsetHeight;this.shapes.forEach((function(t){t.updatePosition(h,r),t.draw(a.canvasContext)})),this.iterationIndex%100==0&&(this.shapes=this.shapes.filter((function(t){return t.getIsVisibleOnCanvas(c)}))),this.lastUpdated=r,this.iterationIndex++,requestAnimationFrame(this.loop)}},{key:"addConfetti",value:function(){for(var t=arguments.length>0&&void 0!==arguments[0]?arguments[0]:{},i=d(t),e=i.confettiRadius,o=i.confettiNumber,n=i.confettiColors,s=i.emojis,a=i.emojiSize,r=window.devicePixelRatio,c=this.canvas.width/r,l=this.canvas.height/r,u=5*l/7,f={x:0,y:u},p={x:c,y:u},m=0;m<o/2;m++)this.shapes.push(new h({initialPosition:f,direction:"right",confettiRadius:e,confettiColors:n,confettiNumber:o,emojis:s,emojiSize:a,canvasWidth:c})),this.shapes.push(new h({initialPosition:p,direction:"left",confettiRadius:e,confettiColors:n,confettiNumber:o,emojis:s,emojiSize:a,canvasWidth:c}))}}]),i}()}();
-
